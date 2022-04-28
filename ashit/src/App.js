fddac29b55425a1a7ab7462df7f5f54a12dadb3d
ashit <ashitkoladiya7@gmail.com>
2022-04-28 21:22:44 +0530

    3 per

--- ashit/src/App.js
+++ ashit/src/App.js
@@ -2,6 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // array function
+  let ashit = [10,"ashit",20,30,40,50]; //normal array
+  
+  
+
+  // ashit.push(99);// add element fast
+
+  // ashit.unshift(99);
+
+  // ashit.pop()//remove element last
+
+  // ashit.shift();
+
+  // ashit.splice(3,0,100);//add element at sefecific position
+
+  // ashit.splice(2,1);//remove element from 2nd index
+
+  // console.log(ashit.toString());
+  // console.log(ashit);
+
+  // let res = ashit.some((a)=> a>10);//check array return true / false
+
+// let res =ashit.find ((a)=> a>10);//check array within array return first match element
+
+// let res =Array.isArray(ashit);// check given element is array or  not
+console.log(res);
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
